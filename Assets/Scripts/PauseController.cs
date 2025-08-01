95c685c7c1b18ef382c368b52e59b691efaec785
AlexW2311 <alex.williams2311@icloud.com>
2025-07-31 23:40:39 -0700

    New update system

--- Assets/Scripts/PauseController.cs
+++ Assets/Scripts/PauseController.cs
@@ -1,16 +1,47 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PauseController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private PlayerControls input;
+    private bool isPaused = false;
+
+    void Awake()
     {
-        
+        input = new PlayerControls();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable()
     {
-        
+        input.Enable();
+        input.Gameplay.Pause.performed += OnPause;
+    }
+
+    void OnDisable()
+    {
+        input.Gameplay.Pause.performed -= OnPause;
+        input.Disable();
+        Time.timeScale = 1f;
+    }
+
+    void OnPause(InputAction.CallbackContext context)
+    {
+        if (!isPaused)
+        {
+            isPaused = true;
+            Time.timeScale = 0f;
+            input.Gameplay.Disable();         // mute ALL gameplay actions
+            input.Gameplay.Pause.Enable();
+
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            isPaused = false;
+            input.Gameplay.Enable();
+        }
+        // Cursor.lockState = isPaused ? CursorLockMode.None : CursorLockMode.Locked;
+        // Cursor.visible = isPaused;
     }
 }
